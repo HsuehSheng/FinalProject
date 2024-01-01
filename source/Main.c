f39d5f6e8211a91b6d1633d4e447f2b93135f09a
HsuehSheng <a0905678648@gmail.com>
2024-01-02 02:04:28 +0800

    merge

--- source/Main.c
+++ source/Main.c
@@ -4,8 +4,22 @@
 
 int main(void)
 {
-	Poker_1();
-	Poker_2();
+	int c;
+	printf("Do you want to play \"10.30\" or \"21.\"(0/1):");
+	scanf("%d", &c);
+
+	switch (c) {
+	case 0:
+		Poker_1();
+		break;
+	case 1:
+		Poker_2();
+		break;
+	default:
+		printf("Enter 0 or 1\n");
+	}
+	
+	
 	system("pause");
 	printf("\n");
 	return 0;
