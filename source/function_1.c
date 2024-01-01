f715867d3d7b36cb617fb9c4aa6de9378bfa15f2
TienChengHung <dicd8151@gmail.com>
2024-01-01 23:00:33 +0800

    third

--- source/function_1.c
+++ source/function_1.c
@@ -5,22 +5,20 @@
 typedef enum { HEARTS, DIAMONDS, CLUBS, SPADES } Suit;
 
 typedef struct {
-	int value;  // 使用整數型別表示點數
+	int value;
 	Suit suit;
 } Card;
 
 Card draw_card(Card* deck, int* num_cards_in_deck) {
 	int random_index;
 	if (*num_cards_in_deck == 0) {
-		// 牌堆用完，回傳一張空的卡牌
-		Card empty_card = { 0, HEARTS }; // 或其他預設值
+		Card empty_card = { 0, HEARTS };
 		return empty_card;
 	}
 
-	random_index = rand() % (*num_cards_in_deck); // 隨機選擇一張卡牌的索引
-	Card drawn_card = deck[random_index]; // 從牌堆中取得卡牌
+	random_index = rand() % (*num_cards_in_deck);
+	Card drawn_card = deck[random_index];
 
-	// 將最後一張卡牌移到被選擇的位置，縮減牌堆大小
 	deck[random_index] = deck[*num_cards_in_deck - 1];
 	(*num_cards_in_deck)--;
 
@@ -60,12 +58,12 @@ void print_card(Card card) {
 	}
 }
 
-float calculate_score(Card cards[], int num_cards) {
+float calculate_score(Card cards[], int num_cards) {	
 	float score = 0;
 
 	for (int i = 0; i < num_cards; ++i) {
 		if (cards[i].value == 11 || cards[i].value == 12 || cards[i].value == 13) {
-			score += 0.5;  // J、Q、K的點數為0.5
+			score += 0.5;
 		}
 		else {
 			score += cards[i].value;
@@ -78,7 +76,7 @@ float calculate_score(Card cards[], int num_cards) {
 void print_cards(Card cards[], int num_cards, int hide_last_card) {
 	for (int i = 0; i < num_cards; ++i) {
 		if (hide_last_card && i == num_cards - 1) {
-			printf("### "); // 顯示隱藏的牌
+			printf("### ");
 		}
 		else {
 			print_card(cards[i]);
@@ -89,13 +87,12 @@ void print_cards(Card cards[], int num_cards, int hide_last_card) {
 }
 
 int Poker_1() {
-	srand((unsigned int)time(NULL));  // 初始化隨機數生成器
+	srand((unsigned int)time(NULL));
 
 	const int max_cards = 52;
 	Card* deck = (Card*)malloc(max_cards * sizeof(Card));
 	int num_cards_in_deck = 0;
 
-	// 初始化牌堆
 	for (int suit = HEARTS; suit <= SPADES; ++suit) {
 		for (int value = 1; value <= 13; ++value) {
 			deck[num_cards_in_deck].value = value;
@@ -109,27 +106,22 @@ int Poker_1() {
 	do {
 		Card player_cards[10];
 		Card computer_cards[10];
-		int num_player_cards = 1; // 一開始只有一張牌
+		int num_player_cards = 1;
 		int num_computer_cards = 1;
 
-		// 開局發牌
 		player_cards[0] = draw_card(deck, &num_cards_in_deck);
 		if (player_cards[0].value == 0) {
-			// 牌堆用完，結束遊戲
 			break;
 		}
 		printf("你抽到的牌：");
 		print_card(player_cards[0]);
 		printf("\n");
 
-		// 電腦的首張牌
 		computer_cards[0] = draw_card(deck, &num_cards_in_deck);
 		if (computer_cards[0].value == 0) {
-			// 牌堆用完，結束遊戲
 			break;
 		}
 
-		// 玩家選擇是否加牌
 		while (1) {
 			float player_score = calculate_score(player_cards, num_player_cards);
 			float computer_score = calculate_score(computer_cards, num_computer_cards);
@@ -139,7 +131,7 @@ int Poker_1() {
 			printf("目前點數：%.1f\n", player_score);
 
 			printf("電腦的牌：");
-			print_cards(computer_cards, num_computer_cards, 1); // 隱藏電腦的最後一張牌
+			print_cards(computer_cards, num_computer_cards, 1);
 			printf("\n");
 
 			printf("是否要加牌？(y/n): ");
@@ -148,7 +140,6 @@ int Poker_1() {
 			if (should_continue == 'y') {
 				player_cards[num_player_cards] = draw_card(deck, &num_cards_in_deck);
 				if (player_cards[num_player_cards].value == 0) {
-					// 牌堆用完，結束遊戲
 					break;
 				}
 				printf("你抽到的牌：");
@@ -160,24 +151,20 @@ int Poker_1() {
 				break;
 			}
 
-			// 檢查玩家是否爆牌
 			if (calculate_score(player_cards, num_player_cards) > 10.5) {
 				printf("你爆牌了，電腦獲勝！\n");
 				goto end_game;
 			}
 		}
 
-		// 電腦選擇是否加牌
 		while (calculate_score(computer_cards, num_computer_cards) < 8.5) {
 			computer_cards[num_computer_cards] = draw_card(deck, &num_cards_in_deck);
 			if (computer_cards[num_computer_cards].value == 0) {
-				// 牌堆用完，結束遊戲
 				break;
 			}
 			num_computer_cards++;
 		}
 
-		// 顯示最終結果
 		printf("你的牌：");
 		print_cards(player_cards, num_player_cards, 0);
 		printf("總點數：%.1f\n", calculate_score(player_cards, num_player_cards));
@@ -186,7 +173,6 @@ int Poker_1() {
 		print_cards(computer_cards, num_computer_cards, 0);
 		printf("總點數：%.1f\n", calculate_score(computer_cards, num_computer_cards));
 
-		// 判斷勝負
 		if (calculate_score(player_cards, num_player_cards) > 10.5) {
 			printf("你爆牌了，電腦獲勝！\n");
 		}
@@ -204,7 +190,6 @@ int Poker_1() {
 		}
 
 	end_game:
-		// 繼續遊戲的選項
 		printf("是否繼續遊戲？(y/n): ");
 		scanf(" %c", &should_continue);
 
